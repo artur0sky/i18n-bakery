543b27309d879718bbe7c43cbc64142a28796cd0
Arturo Sáenz <saenzsanchezblack@hotmail.com>
2025-12-08 14:48:07 -0600

    Update main.tsx

--- examples/react-basic/src/main.tsx
+++ examples/react-basic/src/main.tsx
@@ -1,29 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { I18nProvider } from '@i18n-bakery/react';
-import { HttpBackend, NumberFormatPlugin, CapitalizePlugin } from '@i18n-bakery/core';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { I18nProvider } from "@i18n-bakery/react";
+import {
+  HttpBackend,
+  NumberFormatPlugin,
+  CapitalizePlugin,
+} from "@i18n-bakery/core";
 
 // Initialize plugins
-const httpBackend = new HttpBackend({
-  loadPath: '/locales/{{lng}}/{{ns}}.json'
-});
+const httpBackend = new HttpBackend();
 
 const numberFormat = new NumberFormatPlugin();
 const capitalize = new CapitalizePlugin();
 
 const config = {
-  locale: 'en-US',
-  fallbackLocale: 'en-US',
-  supportedLocales: ['en-US', 'es-MX', 'it', 'jp'],
-  defaultNamespace: 'common', // Set default namespace to 'common'
+  locale: "en-US",
+  fallbackLocale: "en-US",
+  supportedLocales: ["en-US", "es-MX", "it", "jp"],
+  defaultNamespace: "common", // Set default namespace to 'common'
   plugins: [httpBackend, numberFormat, capitalize], // Register plugins
   loader: httpBackend,
-  debug: true
+  debug: true,
 };
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <I18nProvider config={config}>
       <App />
